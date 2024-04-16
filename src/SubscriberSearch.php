3d6740f47795ad59308d301842111a5375587ccc
SIJED <Danilcarol56@gmail.com>
2024-04-16 21:14:15 +0700

    Initial commit

---
+++ src/SubscriberSearch.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Controller;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+
+class SubscriberSearch
+{
+    public static function search(EloquentBuilder $query, $searchQuery)
+    {
+        return $query->where(function ($query) use ($searchQuery) {
+            $query->whereRaw("LOWER(name) LIKE '%".strtolower($searchQuery)."%'")
+                ->orWhereRaw("LOWER(surname) LIKE '%".strtolower($searchQuery)."%'")
+                ->orWhereRaw("LOWER(patronymic) LIKE '%".strtolower($searchQuery)."%'");
+        })->get();
+    }
+}
